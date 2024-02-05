72a5f3ec5e1285c62e41d5f3442395b5a7bea66c
jerome provensal <jeromegit@provensal.com>
2024-02-04 21:31:52 -0800

    * Add tail -f to terminal widget

--- bbg_emsx_simulator/panel_webapp.py
+++ bbg_emsx_simulator/panel_webapp.py
@@ -1,4 +1,7 @@
+import re
+import subprocess
 import time
+from collections import deque
 from typing import List, Union, Any
 
 import panel as pn
@@ -10,21 +13,23 @@ from panel.models.tabulator import CellClickEvent
 from fix_application import FIXApplication
 from order_manager import OrderManager
 
-GRID_KEY = 'grid'
+FIX_SERVER_LOG_FILE_PATH = 'log/FIX.4.2-FIXSERVER-FIXCLIENT.messages.current.log'
 VALID_TICKERS: List[str] = list(FIXApplication.KNOWN_SYMBOLS_BY_TICKER.keys())
 VALID_SIDES: List[str] = list(OrderManager.SIDES)
 PUSH_BUTTON = 'push'
+TAIL_LINE_COUNT = 10
 
 # -- Init
 order_manager = OrderManager()
 order_grid_df = order_manager.create_orders_df_copy()
+last_tailed_log_line = ''
 
 
 # -- Callbacks
 def refresh(_):
     order_manager.read_orders_from_file()
     set_grid_order_df(order_manager.create_orders_df_copy())
-#    log_to_pane(f"Refresh: id:{id(order_grid_df)}:\n {order_grid_df}")
+    #    log_to_pane(f"Refresh: id:{id(order_grid_df)}:\n {order_grid_df}")
     last_update = order_manager.get_file_timestamp()
     log_to_pane(f"Order grid was refreshed from the file (last_update: {last_update})")
 
@@ -37,11 +42,14 @@ def add_row(_):
     order_grid_df.loc[len(order_grid_df)] = new_row
     OrderManager.normalize_orders_df_col_types(order_grid_df)
     set_grid_order_df(order_grid_df)
+
+
 #    log_to_pane(f"order_grid.value. id:{id(order_grid.value)}:\n {order_grid.value}")
 
 def delete_row(_):
     # TODO: Allow to delete rows?
     pass
+
 
 def log_to_pane(text: Union[str, Any]) -> None:
     if not isinstance(text, str):
@@ -61,6 +69,36 @@ def push_order_row(e: CellClickEvent):
 
 def update_theme(e):
     order_grid.theme = e.new
+
+
+def xterm_highlight_str(string: str) -> str:
+    return '\x1b[1;32m' + string + '\x1b[1;37m'
+
+
+def tail_server_fix_log_in_terminal():
+    global last_tailed_log_line
+    # TODO. Need a more efficient way to do this. Remember the last file position and reread from it?
+    command = f"grep -v '35=0' {FIX_SERVER_LOG_FILE_PATH} | tail -{TAIL_LINE_COUNT}"
+    tailed_lines = run_command(command)
+    tailed_lines = [line.replace('\001', '|') for line in tailed_lines if line]
+    if last_tailed_log_line != tailed_lines[-1]:
+        terminal.clear()
+        highlighted_35 = [re.sub('(35=.)', lambda m: xterm_highlight_str(m.group(1)), line) for line in tailed_lines]
+        terminal.write("\n".join(highlighted_35))
+        last_tailed_log_line = tailed_lines[-1]
+
+
+def run_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    if error:
+        print(f"Error: {error}")
+    return output.decode().split('\n')
+
+
+def tail(filename, n=10):
+    with open(filename, 'r') as f:
+        return deque(f, n)
 
 
 def create_order_grid(order_grid_df):
@@ -119,12 +157,14 @@ add_row_button.on_click(add_row)
 refresh_button = pn.widgets.Button(name='Refresh')
 refresh_button.on_click(refresh)
 
-log_title = pn.pane.Markdown("## FIX Log")
+log_title = pn.pane.Markdown(f"## Server FIX Log (last {TAIL_LINE_COUNT} lines)")
 terminal = pn.widgets.Terminal(
     "FIX server app log coming up here... (TBD)\n\n",
-    options={"cursorBlink": True},
-    height=300, sizing_mode='stretch_width'
+    options={"fontSize": 12},
+    height=500, sizing_mode='stretch_width'
 )
+
+pn.state.add_periodic_callback(tail_server_fix_log_in_terminal, period=1_000)
 
 table_theme = pn.widgets.Select(name='Select',
                                 options=['simple', 'default', 'midnight', 'site', 'modern', 'bootstrap',
